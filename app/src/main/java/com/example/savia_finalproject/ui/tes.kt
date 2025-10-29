83d8bfb47c42113a4e7752d060d3265ddc3ac26d
Nadhif Adita <nadijfer02@gmail.com>
2025-10-29 22:01:44 +0700

    tes commit

---
+++ app/src/main/java/com/example/savia_finalproject/ui/tes.kt
@@ -0,0 +1,15 @@
+package com.example.savia_finalproject.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun Tes() {
+    Text(
+        "Rp 0",
+        color = Color.Companion.White,
+        fontWeight = FontWeight.Companion.Bold
+    )
+}
