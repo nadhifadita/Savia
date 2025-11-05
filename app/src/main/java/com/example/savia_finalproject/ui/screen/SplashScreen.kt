044847e3333ecdc4d9708d626672774367fcb13f
Fikri <khairumamfikri2004@gmail.com>
2025-11-05 08:39:50 +0700

    ngetes aja

--- app/src/main/java/com/example/savia_finalproject/ui/screen/SplashScreen.kt
+++ app/src/main/java/com/example/savia_finalproject/ui/screen/SplashScreen.kt
@@ -1,16 +1,15 @@
 package com.example.savia_finalproject.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
+import com.example.savia_finalproject.R
 
 @Composable
 fun SplashScreen(
@@ -28,8 +27,14 @@ fun SplashScreen(
             .padding(24.dp),
         contentAlignment = Alignment.Center
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource( id = R.drawable.logo),
+                contentDescription = "Logo Aplikasi",
+                modifier = Modifier.size(300.dp)
+            )
 
         }
     }
